2e1c3652ce1d5115f9279985589c1d5febed2b08
Orestis Markou <orestis@orestis.gr>
2021-02-08 15:23:55 +0100

    use sente event loop

--- src/nvimgui/client.cljs
+++ src/nvimgui/client.cljs
@@ -23,9 +23,50 @@
   (def chsk-state state)   ; Watchable, read-only atom
   )
 
+(defmulti -event-msg-handler
+  "Multimethod to handle Sente `event-msg`s"
+  :id ; Dispatch on event-id
+  )
+
+(defn event-msg-handler
+  "Wraps `-event-msg-handler` with logging, error catching, etc."
+  [{:as ev-msg :keys [id ?data event]}]
+  (-event-msg-handler ev-msg))
+
+(defmethod -event-msg-handler
+  :default ; Default/fallback case (no other matching handler)
+  [{:as ev-msg :keys [event]}]
+  (js/console.log "Unhandled event: " event))
+
+(defmethod -event-msg-handler :chsk/state
+  [{:as ev-msg :keys [?data]}]
+  (let [[old-state-map new-state-map] ?data]
+    (if (:first-open? new-state-map)
+      (js/console.log "Channel socket successfully established!: " new-state-map)
+      (js/console.log "Channel socket state change: "              new-state-map))))
+
+(defmethod -event-msg-handler :chsk/recv
+  [{:as ev-msg :keys [?data]}]
+  (js/console.log "Push event from server: %s" ?data))
+
+(defmethod -event-msg-handler :chsk/handshake
+  [{:as ev-msg :keys [?data]}]
+  (let [[?uid ?csrf-token ?handshake-data] ?data]
+    (js/console.log "Handshake: " ?data)))
+
+(defonce router_ (atom nil))
+(defn  stop-router! [] (when-let [stop-f @router_] (stop-f)))
+(defn start-router! []
+  (stop-router!)
+  (reset! router_
+    (sente/start-client-chsk-router!
+      ch-chsk event-msg-handler)))
+
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
   (js/console.log "start")
+  (start-router!)
+  #_
   (go-loop []
            (let [x (<! ch-chsk)]
              (println "Got a value in this loop:" x))
@@ -40,4 +81,5 @@
 
 ;; this is called before any code is reloaded
 (defn ^:dev/before-load stop []
-  (js/console.log "stop"))
+  (js/console.log "stop")
+  (stop-router!))
